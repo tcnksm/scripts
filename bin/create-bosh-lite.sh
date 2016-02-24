92a97fd006063b83841aeb09c7287a9b80718431
tcnksm <nsd22843@gmail.com>
2016-02-24 15:32:29 +0900

    Update script

--- bin/create-bosh-lite.sh
+++ bin/create-bosh-lite.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-echo "----> Destory bosh-lite"
 cd ${HOME}/src/github.com/cloudfoundry/bosh-lite
+
+echo "----> Destory bosh-lite"
 vagrant destroy --force
 
 echo "----> Fetch latest source"
 git pull
 
 echo "----> Start bosh-lite"
-# vagrant up --provider=virtualbox
+vagrant up --provider=virtualbox
 
 echo "----> Add route"
 sudo bin/add-route
 
-echo "---> Target"
-TARGET="192.168.59.4"
-bosh target ${TARGET} lite
+echo "----> Target"
+bosh target 192.168.50.4 lite
 
 echo "---> Login as admin"
 bosh login admin admin
 
 echo "----> Upload stemcell"
-bosh upload stemcell ~/.stemcell/bosh-stemcell-389-warden-boshlite-ubuntu-trusty-go_agent.tgz
+bosh upload stemcell --skip-if-exists ~/.stemcell/bosh-stemcell-389-warden-boshlite-ubuntu-trusty-go_agent.tgz
 bosh stemcells
